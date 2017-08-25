39c9e4d7c6bc22197d35415f36e5230d4f0476ec
MAX MCFARLAND <mmcfarland522@gmail.com>
2017-08-25 16:22:53 -0700

    added equals sign

--- js/app-cart.js
+++ js/app-cart.js
@@ -81,7 +81,7 @@ function deleteItemFromCart (event) {
   var tableParent = rowToDelete.parentNode;
   tableParent.removeChild(rowToDelete);
   for (var i = 0; i < JSON.parse(localStorage.main).length; i++){
-    if (JSON.parse(localStorage.main)[i][0] = event.currentTarget.id){
+    if (JSON.parse(localStorage.main)[i][0] == event.currentTarget.id){
       var updateStorage = JSON.parse(localStorage.main);
       updateStorage.splice(i, 1);
       localStorage.setItem('main', JSON.stringify(updateStorage));
